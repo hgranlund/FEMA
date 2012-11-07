380f720cd74136fe82bbe02bfeebd4e26296a785
Simen Haugerud Granlund <simen@hgranlund.com>
2012-11-07 15:07:38 +0100

    started with C

---
+++ src/fortran/FEM.f90
@@ -0,0 +1,86 @@
+program FEM
+  use FEMMethods
+  implicit none
+  integer ::  errorFlag,status,i,file_in,file_out
+  real , ALLOCATABLE :: DisplacementVector(:)
+  type (element),ALLOCATABLE :: Elms(:)
+  type (node), ALLOCATABLE :: Nodes(:)
+  type (load), ALLOCATABLE :: Loads(:)
+
+
+  file_in=5
+  file_out=6
+  errorFlag=0
+
+!   file_in=10
+!   file_out=11
+!   open(unit=file_in, file='inputE52S224.dat', iostat=errorFlag, status="old", action="read")
+!   if ( errorFlag /= 0 ) stop "Error opening file_in"
+!   open(unit=file_out, file='output.dat', iostat=errorFlag, status="unknown", action="write")
+!   if ( errorFlag /= 0 ) stop "Error opening file_out"
+  
+
+
+  call ReadInput() 
+  allocate (DisplacementVector(totalDegrees(Nodes)), stat=errorFlag)
+  IF (errorFlag .NE. 0)then
+     print *, "***Not Enough Memory*** when allocating Displacement "
+     return
+  end IF
+  
+  call DoFEA(DisplacementVector,Elms,Nodes,Loads, Errorflag)
+  if (errorFlag .NE. 0 ) then 
+     print *,'Errorflag = ' ,Errorflag
+     stop
+  end if
+   call WriteOutput()
+
+contains
+  Subroutine ReadInput()
+    integer ::n, numberOfNodes, numberOfElm,numberOfLoads
+    type (element) :: elm
+
+    read(file_in,*,iostat=errorFlag) numberOfNodes, numberOfElm, numberOfLoads
+    allocate (Nodes(numberOfNodes))
+    allocate (Elms(numberOfElm))
+    allocate (Loads(numberOfLoads))
+    read (file_in,*,iostat=errorFlag) (Nodes(n), n=1,numberOfNodes)
+    read (file_in,*, iostat=errorFlag) (Elms(n)%e,Elms(n)%a,Elms(n)%i,Elms(n)%node1,Elms(n)%node2,n=1,numberOfElm)
+    read (file_in,*, iostat=errorFlag) (Loads(n),n=1,numberOfLoads)
+    if ( errorFlag /= 0 ) stop "Read error in file file_in"
+
+    if (pr_switch >= 5 ) then 
+       print *,'ReadInput:'
+       print *, 'Antall noder........: ', numberOfNodes 
+       print *, 'Antall elementer....: ', numberOfElm 
+       print *, 'Antall krefter......: ', numberOfLoads
+    end if
+
+  end Subroutine ReadInput
+
+  Subroutine WriteOutput()
+    integer ::n
+    write (file_out,*,iostat=errorFlag), '==============================================================='
+    write (file_out,*,iostat=errorFlag), '==============================================================='
+    write(file_out,*, iostat=errorFlag) 'Forskyvningsvector'
+    write(file_out, *, iostat=errorFlag) DisplacementVector
+    write (file_out,*,iostat=errorFlag), ''
+    write (file_out,*,iostat=errorFlag), '==============================================================='
+    write(file_out,*, iostat=errorFlag) 'ElementNr:' 
+    write(file_out,*, iostat=errorFlag) '  Areal,          Lengde,         Cos(theta),       Node1,        Node2'
+    write(file_out,*, iostat=errorFlag) '  Fx1,            Fy1,            Mr1;               Fx2,          Fy2,           Mr2 '
+    write(file_out,*, iostat=errorFlag) ''
+    
+
+    do n=1, ubound(Elms,1)
+      write(file_out,*, iostat=errorFlag) 'ElementNr:', n
+      write(file_out,*, iostat=errorFlag) Elms(n)%a,Elms(n)%l,Elms(n)%cosT,Elms(n)%node1,Elms(n)%node2
+      write(file_out,*, iostat=errorFlag) Elms(n)%ForceVector
+      write(file_out,*, iostat=errorFlag) ''
+
+    end do
+       if ( errorFlag /= 0 ) stop "Write error in file file_out"
+
+  end Subroutine WriteOutput
+
+end program FEM
---
+++ src/fortran/FEMUtility.f90
@@ -0,0 +1,88 @@
+module FEMUtility
+
+	implicit none
+
+	   ! pr_switch brukes til å bestemme hva som skal printes ut.  0= ingen ting, 10 = alt 
+
+    integer , parameter :: pr_switch=0
+
+	contains 
+
+
+  !###############################
+  !PrintMatrix printer Matrisen A
+  !
+  ! Author: Simen Haugerud Granlund
+  ! Date/version: 02-11-12/ 1.0
+  !###############################
+
+   Subroutine PrintMatrix(A)
+    real, intent(inout) :: A(:,:)
+
+    integer i,j
+
+    print * , ' #######################################'
+    do i=lbound(A,1), ubound(A,1)
+       print *,(A(i,j)/66670, j=lbound(A,2), ubound(A,2))
+    end do
+    print *, '####################################### '
+    print *,''
+  end Subroutine PrintMatrix
+
+
+
+  !###############################
+  !NullifyRealMatrix Setter alle elementene i Matrix til null
+  !
+  ! Author: Simen Haugerud Granlund
+  ! Date/version: 02-11-12/ 1.0
+  !###############################
+
+  subroutine NullifyRealMatrix(Matrix)
+    real, intent(inout) :: Matrix(:,:) 
+
+    integer i,j
+
+    forall (i=lbound(Matrix,1):ubound(Matrix,1), j=lbound(Matrix,2):ubound(Matrix,2)) Matrix(i,j)=0
+    
+  end subroutine NullifyRealMatrix
+
+
+  !###############################
+  !NullifyRealVector Setter alle elementene i vector til null
+  !
+  ! Author: Simen Haugerud Granlund
+  ! Date/version: 02-11-12/ 1.0
+  !###############################
+
+  subroutine NullifyRealVector(Vector)
+    real, intent(inout) :: Vector(:) 
+
+    integer i
+
+    forall (i=lbound(Vector,1):ubound(Vector,1)) Vector(i)=0
+    
+  end subroutine NullifyRealVector
+
+
+  !###############################
+  ! SwapRow bytter rad r1 med r2 i matrisen A
+  !
+  ! Author: Simen Haugerud Granlund
+  ! Date/version: 02-11-12/ 1.0
+  !###############################
+
+  subroutine SwapRow(A,r1,r2)
+    real, intent(inout) :: A(:,:)
+    integer, intent(in) :: r1,r2
+
+!     integer:: n = size(A, 1)
+    real :: swap(size(A, 1))
+
+    swap=A(r1,:)
+    A(r1,:)=A(r2,:)
+    A(r1,:)=swap
+
+  end subroutine swapRow
+
+end module FEMUtility
